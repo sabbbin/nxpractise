8c6a866ce3bc555f9abab163ca3a37ff3aa788b0
sabbbin <suwal721063@gmail.com>
2023-07-01 20:42:33 +0545

    fix: fixing knex problem

--- libs/knexjs/src/lib/knexConfiguration.ts
+++ libs/knexjs/src/lib/knexConfiguration.ts
@@ -1,5 +1,5 @@
 import { ConfigurableModuleBuilder } from '@nestjs/common';
-import { Knex } from 'knex';
+
 import { KnexModuleOptions } from './constantKnex';
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE } =
--- libs/knexjs/src/lib/knexjs.module.ts
+++ libs/knexjs/src/lib/knexjs.module.ts
@@ -1,37 +1,51 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { ConfigurableModuleClass, OPTIONS_TYPE } from './knexConfiguration';
+import { DynamicModule, Inject, Module, Provider } from '@nestjs/common';
+import {
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN,
+  OPTIONS_TYPE,
+} from './knexConfiguration';
 import knex, { Knex } from 'knex';
 import { DefaultKnexProvider } from './constantKnex';
 
-@Module({
-  controllers: [],
-  providers: [],
-  exports: [],
-})
+@Module({})
 export class KnexjsModule extends ConfigurableModuleClass {
-  static root(options: typeof OPTIONS_TYPE): DynamicModule {
+  static forRoot(options: typeof OPTIONS_TYPE): DynamicModule {
+    console.log('helo');
     const dynamicModule = super.forRoot(options);
-    const knexI = createKnexInstance(options?.token, options.options);
+    const knexI = this.createKnexInstance(options?.token, options.options);
 
     return {
       global: true,
+      imports: dynamicModule.imports,
       module: dynamicModule.module,
-
       providers: [...(dynamicModule.providers ?? []), knexI],
+      exports: [knexI],
+    };
+  }
+
+  private static createKnexInstance(
+    token: string | undefined,
+    options: Knex.Config
+  ): Provider {
+    return {
+      provide: DefaultKnexProvider,
+
+      useFactory: () => {
+        const knexInstance = knex(options);
+        knexInstance
+          .raw('select 1')
+          .then(() => {
+            console.log('knex connected');
+          })
+          .catch((e) => {
+            console.log('error in connect knex' + e);
+          });
+        return knexInstance;
+      },
     };
   }
 }
 
-export const createKnexInstance = (
-  token: string | undefined,
-  options: Knex.Config
-) => {
-  const temp = token ? token : DefaultKnexProvider;
-  return {
-    provide: temp,
-    useFactory: () => {
-      const knexInstance = knex(options);
-      return knexInstance;
-    },
-  };
+export const InjectKnex = () => {
+  return Inject(MODULE_OPTIONS_TOKEN);
 };
